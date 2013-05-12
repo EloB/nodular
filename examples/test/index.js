e6088a35bb63eea8c47de565ffd59ba9fe4b4109
Olle Bröms <olle.broms@ewebbyran.se>
2013-05-13 01:00:42 +0200

    Added a new example.

--- examples/test/index.js
+++ examples/test/index.js
@@ -1,5 +1,27 @@
 var nodular = require('../../index');
 
-module.exports = function(callback) {
-	nodular(require, callback);
-};
+nodular(require, function(
+  error
+, $nodularStackparser
+, file
+, fileWithDash
+, coffeeFile
+, coffeeFileWithDash
+, multiple
+) {
+	if(error) throw error;
+	
+	console.log($nodularStackparser);
+	console.log(file);
+	console.log(fileWithDash);
+	console.log(coffeeFile);
+	console.log(coffeeFileWithDash);
+	
+	// Files are not required before used
+	console.log(multiple);
+	
+	// Use __requireAll() to require all files
+	multiple.__requireAll();
+	
+	console.log(multiple);
+});
